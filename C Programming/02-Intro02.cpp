30630d6c1ac0c7bd22ece4b7d8f64dc226488322
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2020-05-13 00:16:50 +0700

    Add files via upload

---
+++ C Programming/02-Intro02.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main ()
+{
+    char a,b;
+    scanf("%c %c",&a,&b);
+    //printf("%c %c",tolower(a),toupper(b));
+    //printf("%d",'a'-'A');
+    printf("%c %c",a+32,b-32);
+    return 0;
+}
