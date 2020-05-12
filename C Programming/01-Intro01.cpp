48345a6b2f6ba97a50b2eea804f871690611ee44
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2020-05-13 00:05:24 +0700

    Add files via upload

---
+++ C Programming/01-Intro01.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    printf("hello, world");
+    printf("\n");
+    printf("'\\n' is a New line character.");
+    printf("\n");
+    printf("'\\t' is a Tab character.");
+    printf("\n");
+    printf("4%3==1 but 4%2!=1");
+    return 0;
+}
