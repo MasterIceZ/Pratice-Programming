4dec5395d6ebae46a6d5b43d2c9a48e7e3d15620
AiceAeng <64594596+MasterIceZ@users.noreply.github.com>
2020-06-12 14:45:20 +0700

    Roam Dp Version

--- Problems/24-Roam.cpp
+++ Problems/24-Roam.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int mine[100100];
 int main () {
+    /**
     int n,q,i,num,l,r;
     scanf("%d %d",&n,&q);
     while(q--){
@@ -12,6 +13,23 @@ int main () {
     }
     scanf("%d",&q);
     int index;
+    while(q--){
+        scanf("%d",&index);
+        printf("%d\n",mine[index]);
+    }
+    */
+    int n,q,l,r;
+    scanf("%d %d",&n,&q);
+    while(q--){
+        scanf("%d %d",&l,&r);
+        mine[l]++;
+        mine[r+1]--;
+    }
+    for(int i=1;i<=n;i++){
+        mine[i] += mine[i-1] ;
+    }
+    int index;
+    scanf("%d",&q);
     while(q--){
         scanf("%d",&index);
         printf("%d\n",mine[index]);
