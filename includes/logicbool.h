9312a2ccceac454bdd2d8bc4f1ca9a2d26b44c14
IceBorworntat <64594596+MasterIceZ@users.noreply.github.com>
2020-07-25 10:12:32 +0700

    logib

---
+++ includes/logicbool.h
@@ -0,0 +1,31 @@
+/*
+ * Logic Operations
+ * IceBorworntat
+ * */
+bool andl (bool a,bool b)
+{
+	if(a==true && b==true)
+		return true;
+	return false;
+}
+
+bool orl (bool a,bool b)
+{
+	if(a==false && b==false)
+		return false;
+	return true;
+}
+
+bool ifthenl (bool a,bool b)
+{
+	if(a==true && b==false)
+		return false;
+	return true;
+}
+
+bool whenl (bool a,bool b)
+{
+	if(a==b)
+		return true;
+	return false;
+}
