4a23830eda7c88d7f81a18071cb5a9ff86caa60a
IceBorworntat <64594596+MasterIceZ@users.noreply.github.com>
2020-07-27 19:32:59 +0700

    Template.cpp

---
+++ Facebook Hacker Cup 2020/tempalte.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long;
+#define pii pair<int,int>
+#define pb push_back
+#define us unsigned
+#define vi vector<int>
+#define endl "\n"
+
+void solve()
+{
+
+	print();
+	return ; 
+}
+
+void print ()
+{
+
+	return ;
+}
+
+int main ()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	int q,i;
+	i = 0;
+	cin >> q;
+	while(q--)
+	{
+		//memset array
+		cout << "Case #" << i+1 << ":" << endl;
+		solve();
+	}
+	return 0;
+}	
