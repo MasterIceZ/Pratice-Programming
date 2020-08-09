b23ca18d23041d9b461c39f7dbe07de641c74852
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-08-09 13:05:50 +0700

    Update Template

--- Facebook Hacker Cup 2020/tempalte.cpp
+++ Facebook Hacker Cup 2020/tempalte.cpp
@@ -1,23 +1,59 @@
+/*
+ *	AUTHOR : _IceBorworntat926
+ * */
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long;
-#define pii pair<int,int>
-#define pb push_back
+#define ll long long
 #define us unsigned
+#define pii pair<int,int>
+#define f0(a,b,c) for(a=b;b<c;a++)
+#define f1(a,b,c) for(a=b;b<=c;a++)
+#define f2(a,b,c) for(a=b;a>c;a--)
+#define f3(a,b,c) for(a=b;a>=c;a--)
+#define r4i {-1,1,0,0}
+#define r4j {0,0,-1,1}
+#define r8i {-1,-1,-1,0,0,1,1,1}
+#define r8j {-1,0,1,-1,1,-1,0,1}
+#define INF 1e9
+#define arr array
+#define MOD 1e9+7
+#define l long
+#define str string
+#define st struct
+#define def void
+#define qi queue<int>
+#define pb(a) push_back(a)
+#define pq priority_queue
+#define vt vector
 #define vi vector<int>
-#define endl "\n"
+#define infile() freopen("input.in","r",stdin)
+#define outfile() freopen("sol.out","w",stdout)
+#define sz(x) (int)(x).size()
+#define all(c) (c).begin(),(c).end()
+#define in cin
+#define out cout
+#define ld long double
 
-void solve()
+const int d4i[] = r4i,d4j[] = r4j;
+const int d8i[] = r8i,d8j[] = r8j;
+
+void solution()
 {
+	int i;
 
-	print();
-	return ; 
+	return ;
 }
 
-void print ()
+void print()
 {
+	
+	return ;
+}
 
+void debug()
+{
+	
 	return ;
 }
 
@@ -25,16 +61,17 @@ int main ()
 {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-	int q,i;
-	i = 0;
+	int q;
 	cin >> q;
+	int i;
+	i = 1;
+	//q = 1;
 	while(q--)
 	{
-		//memset array
-		cout << "Case #" << i+1 << ":" << endl;
-		solve();
+		cout << "Case #" << i++ << ":";
+		cout << endl;
+		solution();
 	}
+
 	return 0;
-}	
+}
