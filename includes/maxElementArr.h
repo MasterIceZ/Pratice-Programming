79bd0c36ebc17ec3605482fe077344ff0e082fb3
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-08-27 11:39:02 +0700

    Update maxElementArr.h

--- includes/maxElementArr.h
+++ includes/maxElementArr.h
@@ -1,10 +1,12 @@
 
-int mxelement2d(int matrix[][],int n)
+#include <cstdio>
+
+int mxelement2d(int matrix[][],int n ,int m)
 {
 	int i,j,mx=-1e9;
 	for(i=0;i<n;i++)
 	{
-		for(j=0;j<n;j++)
+		for(j=0;j<m;j++)
 		{
 			mx = max(matrix[i][j],mx);
 		}
