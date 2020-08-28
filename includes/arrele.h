383a7e4966575a3419785355f2384641105972b1
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-08-28 19:11:22 +0700

    Array Element

---
+++ includes/arrele.h
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+
+//min element
+template <class T> T mnelement(vector<T> v);
+template <class T> T mnelement(vector<T> v)
+{
+	sort(v.begin(),v.end());
+	return v[0];
+}
+
+template <class T> T mxelement(vector<T> v);
+template <class T> T mxelement(vector<T> v)
+{
+	sort(v.begin(),v.end(),greater<T>());	
+	return v[0];
+}
