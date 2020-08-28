a29fb4293d639107306542419198c5e51a9f87ba
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-08-28 19:55:43 +0700

    Update arrele.h

--- includes/arrele.h
+++ includes/arrele.h
@@ -20,3 +20,21 @@ template <class T> T mxelement(vector<T> v)
 	sort(v.begin(),v.end(),greater<T>());	
 	return v[0];
 }
+
+//find element
+template <class T> int findelement(vector<T> v,T want);
+template <class T> int findelement(vector<T> v,T want)
+{
+    bool found=false;
+    int idx;
+    for(int i=0;i<v.size();++i)
+    {
+        if(want==v[i])
+        {
+            found = true;
+            idx = i;
+            break;
+        }
+    }
+    return found?idx:-1;
+}
