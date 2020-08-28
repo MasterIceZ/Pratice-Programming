5129513619e1b2eabed650c61f1f39aab643e6de
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-08-28 20:04:14 +0700

    Add namespace

--- includes/arrele.h
+++ includes/arrele.h
@@ -5,36 +5,41 @@
 
 using namespace std;
 
-//min element
-template <class T> T mnelement(vector<T> v);
-template <class T> T mnelement(vector<T> v)
-{
-	sort(v.begin(),v.end());
-	return v[0];
-}
 
-//max element
-template <class T> T mxelement(vector<T> v);
-template <class T> T mxelement(vector<T> v)
+namespace ice
 {
-	sort(v.begin(),v.end(),greater<T>());	
-	return v[0];
-}
+	//min element
+	template <class T> T mnelement(vector<T> v);
+	template <class T> T mnelement(vector<T> v)
+	{
+		sort(v.begin(),v.end());
+		return v[0];
+	}
 
-//find element
-template <class T> int findelement(vector<T> v,T want);
-template <class T> int findelement(vector<T> v,T want)
-{
-    bool found=false;
-    int idx;
-    for(int i=0;i<v.size();++i)
-    {
-        if(want==v[i])
-        {
-            found = true;
-            idx = i;
-            break;
-        }
-    }
-    return found?idx:-1;
+	//max element
+	template <class T> T mxelement(vector<T> v);
+	template <class T> T mxelement(vector<T> v)
+	{
+		sort(v.begin(),v.end(),greater<T>());
+		return v[0];
+	}
+
+	//find element
+	template <class T> int findelement(vector<T> v,T want);
+	template <class T> int findelement(vector<T> v,T want)
+	{
+    		bool found=false;
+   		int idx;
+    		for(int i=0;i<v.size();++i)
+    		{
+        		if(want==v[i])
+        		{
+            			found = true;
+            			idx = i;
+            			break;
+        		}
+    		}
+    		return found?idx:-1;
+	}
+
 }
