cfd45c4a9b66e96d112eb492adbbda46c948e0cc
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-08-29 08:15:56 +0700

    Deduct O(n) of mxelement and mnelement

--- includes/arrele.h
+++ includes/arrele.h
@@ -1,44 +1,62 @@
-#include <cstdio>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
+
 namespace ice
 {
-	//min element
-	template <class T> T mnelement(vector<T> v);
-	template <class T> T mnelement(vector<T> v)
-	{
-		sort(v.begin(),v.end());
-		return v[0];
-	}
 
-	//max element
-	template <class T> T mxelement(vector<T> v);
-	template <class T> T mxelement(vector<T> v)
-	{
-		sort(v.begin(),v.end(),greater<T>());
-		return v[0];
-	}
+    //min element
+    template <class T>
+    T mnelement(vector<T> v);
+    template <class T>
+    T mnelement(vector<T> v)
+    {
+        for(int i=1;i<v.size();++i)
+        {
+            if(v[i]>v[i-1])
+            {
+                v[i] = v[i-1];
+            }
+        }
+        return v[v.size()-1];
+    }
 
-	//find element
-	template <class T> int findelement(vector<T> v,T want);
-	template <class T> int findelement(vector<T> v,T want)
-	{
-    		bool found=false;
-   		int idx;
-    		for(int i=0;i<v.size();++i)
-    		{
-        		if(want==v[i])
-        		{
-            			found = true;
-            			idx = i;
-            			break;
-        		}
-    		}
-    		return found?idx:-1;
-	}
+    //max element
+    template <class T>
+    T mxelement(vector<T> v);
+    template <class T>
+    T mxelement(vector<T> v)
+    {
+        for(int i=1;i<v.size();++i)
+        {
+            if(v[i]<v[i-1])
+            {
+                v[i] = v[i-1];
+            }
+        }
+        return v[v.size()-1];
+    }
 
-}
+    //find element
+    template <class T>
+    int findelement(vector<T> v, T want);
+    template <class T>
+    int findelement(vector<T> v, T want)
+    {
+        bool found = false;
+        int idx;
+        for (int i = 0; i < v.size(); ++i)
+        {
+            if (want == v[i])
+            {
+                found = true;
+                idx = i;
+                break;
+            }
+        }
+        return found ? idx : -1;
+    }
+
+} // namespace ice
