56a9c959f35c4a942d2b83142e394fb68945015b
_IceBorworntat926 <64594596+MasterIceZ@users.noreply.github.com>
2020-08-31 21:54:03 +0700

    template

---
+++ temp.cpp
@@ -0,0 +1,70 @@
+
+// Tempalte By _IceBorworntat926 
+// Start Line 68
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define INF 2e18
+#define all(x) x.begin(),x.end()
+#define _DBG() cout << "EiEi" << endl
+#define dec(a,b) fixed<<setprecision(b)<<a
+#define endl '\n'
+#define space " "
+#define rep(i,a,b) for(i=a;i<b;++i)
+#define per(i,a,b) for(i=a;i>0;--i)
+#define FOR(i,a,b) for(i=a;i<=b;++i)
+#define ROF(i,a,b) for(i=a;i>=0;--i)
+
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef priority_queue pq;
+typedef struct st;
+
+template <class A> void read(vector<A>& v);
+template <class A,size_t S> void read(array <A, S>& a);
+template <class T> void read(T& x)
+{
+	cin >> x;
+}
+void read(double& d)
+{
+	string t;
+	read(t);
+	d = stod(t);
+}
+void read(long double& d)
+{
+	string t;
+	read(t);
+	d = stold(t);
+}
+template <class H,class... T> void read(H& h,T&... t)
+{
+	read(h);
+	read(t...);
+}
+template <class A> void read(vector <A>& x)
+{
+	for(auto& a : x)
+	{
+		read(a);
+	}
+}
+template <class A,size_t S> void read(array<A, S>& x)
+{
+	for(auto& a : x)
+	{
+		read(x);
+	}
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	//Code Here :
+	
+	return 0;
+}
