f4b380c333160d72ff9e561c79cc68856e96c431
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-10-10 10:17:42 +0700

    Permute

---
+++ Recursive/09-Permutation.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define int long long
+#define pii pair<int,int>
+#define FOR(i,n,k) for(int i=n; i<k; ++i)
+#define ROF(i,n) for(int i=n; i>-1; --i)
+#define V_IN(v); for(auto& x : v){cin >> x;}
+#define V_1IN(v,n) for(int i=1;i<=n;++i){cin>>v[i];}
+int gcd(int a,int b){if(a%b==0)return b; return gcd(b%a,b);}
+int lcm(int a,int b){return a*b/gcd(a,b);}
+std::string reverse(string s){int n = s.size(),cnt=0; std::string rev = s; for(int i=n-1;i>=0;--i)rev[cnt++] = s[i]; return rev;}
+typedef long long ll;
+const int INF = 2e18;
+int n,r,k,cnt=0;
+int ans[12],mark[12];
+void play(int state){
+	if(state>=r){
+		if(cnt==k){
+			for(int i=0;i<r;++i){
+				cout << ans[i] << space;
+			}
+			exit(0);
+		}
+		cnt++;
+		return ;
+	}
+	for(int i=1;i<=n;++i){
+		if(mark[i]){
+			continue;
+		}
+		mark[i]=1;
+		ans[state]=i;
+		play(state+1);
+		mark[i]=0;
+	}
+}
+int32_t main (){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> r >> k;
+	--k;
+	play(0);
+	cout << endl;
+	return 0;
+}
